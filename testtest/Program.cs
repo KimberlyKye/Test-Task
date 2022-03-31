9f72ea200d185958babefe03d575041d8398aa46
melanie <lovelystasik@gmail.com>
2022-04-01 00:40:23 +0300

    Created new proj

---
+++ testtest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace testtest
+{
+    class MainClass
+    {
+        public static async Task Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            string path = "in.put";
+
+            StreamReader reader = null;
+
+            try
+            {
+                reader = new StreamReader(path);
+                string text = await reader.ReadToEndAsync();
+                Console.WriteLine(text);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("{0} Exception caught.", ex);
+            }
+            finally
+            {
+                reader?.Close();
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine("{0} ms", stopwatch.ElapsedMilliseconds);
+        }
+
+     
+    }
+}
